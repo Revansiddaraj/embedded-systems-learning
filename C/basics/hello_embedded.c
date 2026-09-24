81aa23336225ad9f20d0d56da06ce22a36f58e01
Revansidda Rajeshwar <rajeshwarrevansidda21@gmail.com>
2026-09-24 12:47:32 +0530

    Day 1 - Added first C program

---
+++ C/basics/hello_embedded.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, Embedded Systems!\n");
+    printf("My name is Revansidda.\n");
+    printf("Day 1 - C Programming Practice\n");
+
+    return 0;
+}
